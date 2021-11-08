047708476b5a5ca641095b2566feeb24364be524
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2021-11-08 12:41:52 +0100

    feat: added mqtt command to drive

--- car/main.py
+++ car/main.py
@@ -1,1 +1,13 @@
-print("Hello world")
+from MqttClient import MqttClient
+from diddyborgv2.ttm4160_test import PerformDrive
+
+
+def handleCommand(command):
+    if (command['command'] == "go"):
+        print(command)
+        PerformDrive(1)
+
+
+if __name__ == '__main__':
+    client = MqttClient(handleCommand)
+    client.startLoop()
