6a4e03e66a065f8cd7b3d7bfbdfdace079ba7476
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2021-11-08 16:50:11 +0100

    feat: enable lambda

--- infra/src/infra-stack.ts
+++ infra/src/infra-stack.ts
@@ -1,4 +1,8 @@
 import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigateway from "@aws-cdk/aws-apigateway";
+import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
+import * as path from "path";
 import { WebStack } from "../src/web-stack";
 
 export class InfraStack extends cdk.Stack {
@@ -8,5 +12,25 @@ export class InfraStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     const webApp = new WebStack(this, "TTM4160WebStack", {});
+
+    const handler = new lambda.Function(this, "WidgetHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X, // So we can use async in widget.js
+      code: lambda.Code.fromAsset(path.join(__dirname, "../lambda/build")),
+      handler: "index.main",
+      environment: {},
+    });
+
+    const api = new apigateway.RestApi(this, "widgets-api", {
+      restApiName: "TTM4160 service",
+      description: "This service serves widgets.",
+    });
+
+    const getWidgetsIntegration = new apigateway.LambdaIntegration(handler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' },
+    });
+    const postWidgetIntegration = new apigateway.LambdaIntegration(handler);
+
+    api.root.addMethod("GET", getWidgetsIntegration); // GET /
+    api.root.addMethod("POST", postWidgetIntegration); // POST /
   }
 }
