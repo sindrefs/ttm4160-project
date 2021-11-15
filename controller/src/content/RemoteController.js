b0119d3d328152f78a6fb1757b689eee4194cf46
Sindre Sørensen <sindrefs@gmail.com>
2021-11-15 14:47:06 +0100

    Fix catch blocks

--- controller/src/content/RemoteController.js
+++ controller/src/content/RemoteController.js
@@ -21,7 +21,7 @@ const RemoteController = () => {
       method: "POST",
       body: JSON.stringify({ joystick: event, command: "joystick" })
     })
-    .then(response => console.log(response))
+      .then(response => console.log(response))
       .catch(err => console.log(err));
   }
 
@@ -31,9 +31,10 @@ const RemoteController = () => {
       method: "POST",
       body: JSON.stringify({ joystick: event, command: "stop" })
     })
-      
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
   }
-
+  
   return (
     <div>
       <Joystick size={100} throttle={200} baseColor="#232C33" stickColor="red" move={handleMove} stop={handleStop}></Joystick>
