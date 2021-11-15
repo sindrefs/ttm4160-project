7818c9333b614b9f8533ada07c191203c488e867
Sindre Sørensen <sindrefs@gmail.com>
2021-11-15 12:55:32 +0100

    Update long if blabla

--- car/main.py
+++ car/main.py
@@ -3,18 +3,18 @@ from diddyborgv2.ttm4160_test import PerformContinousMove, PerformStop
 
 
 def handleCommand(command):
-    print(command['command'])
-    if (command['command'] == "go"):
-        print(command)
-        PerformContinousMove()
-    elif (command['command'] == "stop"):
-        PerformStop()
-    elif (command['command'] == "backwards"):
-        PerformContinousMove(direction="BACKWARDS")
-    elif (command['command'] == "joystick"):
-        joystickvalues = command['joystick']
-        dir = "FORWARDS" if joystickvalues.y > 0 else "BACKWARDS"
-        PerformContinousMove(joystickvalues.x, dir)
+    if command['command']:
+        if (command['command'] == "go"):
+            print(command)
+            PerformContinousMove()
+        elif (command['command'] == "stop"):
+            PerformStop()
+        elif (command['command'] == "backwards"):
+            PerformContinousMove(direction="BACKWARDS")
+        elif (command['command'] == "joystick"):
+            joystickvalues = command['joystick']
+            dir = "FORWARDS" if joystickvalues.y > 0 else "BACKWARDS"
+            PerformContinousMove(joystickvalues.x, dir)
 
 
 if __name__ == '__main__':
