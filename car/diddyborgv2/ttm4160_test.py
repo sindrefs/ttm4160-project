c199aaabe95e3d2c5f35538c32c89f2487d07d4f
Sindre Sørensen <sindrefs@gmail.com>
2021-11-15 13:12:51 +0100

    Change power ratio

--- car/diddyborgv2/ttm4160_test.py
+++ car/diddyborgv2/ttm4160_test.py
@@ -56,16 +56,16 @@ def PerformStop():
 
 
 def PerformContinousMove(angle=0, direction="FORWARDS"):
-    directionPolarity = 1
+    directionPolarity = -1
     if (direction == "BACKWARDS"):
-        directionPolarity = -1
+        directionPolarity = 1
 
     offset = 0.995
     if angle > 0:  # turn right
         TB.SetMotor1(directionPolarity * maxPower)
-        TB.SetMotor2(directionPolarity * -(maxPower * offset) * (1+(angle/100)))
+        TB.SetMotor2(directionPolarity * -(maxPower * offset) * (1+(angle/50)))
     elif angle < 0:
-        TB.SetMotor1(directionPolarity * maxPower * (1+(angle/100)))
+        TB.SetMotor1(directionPolarity * maxPower * (1+(angle/50)))
         TB.SetMotor2(directionPolarity * -(maxPower * offset))
     else:
         TB.MotorsOff()
